aac09657c6f12a97fcc075945be0d345b7907e04
unknown <huy.sirmaster@gmail.com>
2018-09-16 14:14:57 +0700

    while

--- src/huy/dev/Basic.java
+++ src/huy/dev/Basic.java
@@ -3,10 +3,12 @@ package huy.dev;
 public class Basic {
 
 	public static void main(String[] args) {
-		int tong=0;
-		for(int i=0;i<=100;i++)
-		{ tong=tong+i;
+		int n = 0,tong=0;
+		while (n<=100) {
+			tong=tong+n;
+			n++;
 		}
-		System.out.println("Tong 100 so dau tien la : "+tong);
+		System.out.println("Tong 100 so dau tien la "+tong);
 	}
+
 }
