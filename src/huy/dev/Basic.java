d457b4f0d71434e93da0af0043a6041e6c43f66e
unknown <huy.sirmaster@gmail.com>
2018-09-16 14:14:09 +0700

    for

--- src/huy/dev/Basic.java
+++ src/huy/dev/Basic.java
@@ -1,47 +1,12 @@
 package huy.dev;
 
-import java.util.Scanner;
-
 public class Basic {
 
 	public static void main(String[] args) {
-		int n=5;
-		switch (n) {
-		case 0:
-			System.out.println("zero");
-			break;
-		case 1:
-			System.out.println("one");
-			break;	
-		case 2:
-			System.out.println("two");
-			break;
-		case 3:
-			System.out.println("three");
-			break;
-		case 4:
-			System.out.println("four");
-			break;
-		case 5:
-			System.out.println("five");
-			break;
-		case 6:
-			System.out.println("six");
-			break;
-		case 7:
-			System.out.println("seven");
-			break;
-		case 8:
-			System.out.println("eight");
-			break;
-		case 9:
-			System.out.println("nine");
-			break;
-		case 10:
-			System.out.println("ten");
-			break;
-		default:
-			break;
+		int tong=0;
+		for(int i=0;i<=100;i++)
+		{ tong=tong+i;
 		}
+		System.out.println("Tong 100 so dau tien la : "+tong);
 	}
 }
